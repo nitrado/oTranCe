e9f6fda7f91ce76f8a26b89de3b4b8ae084cc61e
Josef Roth <josef.roth@marbis.net>
2013-12-17 12:42:44 +0100

    added iOS importer

---
+++ modules/library/Import/Ios.php
@@ -0,0 +1,29 @@
+<?php
+
+class Module_Import_Ios implements Msd_Import_Interface {
+
+    public function extract($data) {
+        $extractedData = array();
+
+        $lines = explode("\n", $data);
+        $lines_count  = count($lines);
+
+        for ($i = 0; $i < $lines_count; $i++) {
+            $regEx = '/^"(.*)"\s*=\s*"(.*)";$/';
+            preg_match($regEx, $lines[$i], $currentLine);
+
+            $currentKey = trim($currentLine[1]);
+            if ($currentKey == '' || !isset($currentLine[1])) {
+                continue;
+            }
+            $extractedData[$currentKey] = trim($currentLine[2]);
+        }
+
+        return $extractedData;
+    }
+
+    public function getInfo(Zend_View_Interface $view)
+    {
+        return $view->render('ios.phtml');
+    }
+}
